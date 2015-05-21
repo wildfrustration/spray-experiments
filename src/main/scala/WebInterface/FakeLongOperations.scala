57bd0a0e10b80280183bd43bbc0be1e5e00c05c7
Yordan Dimitrushev <wildfrustration@yahoo.com>
2015-05-20 22:59:26 -0700

    wow

--- src/main/scala/WebInterface/FakeLongOperations.scala
+++ src/main/scala/WebInterface/FakeLongOperations.scala
@@ -5,7 +5,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object FakeLongOperations {
 
-  def inTheFuture(seconds: Int) = Future {
+  def inTheFuture(seconds: Long) = Future {
     blocking(Thread.sleep(seconds * 1000))
     seconds
   }
